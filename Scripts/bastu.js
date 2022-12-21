838b1058fc52de0ccb74747df87c65091680502f
Mattias Lindblad <mattias-lindblad2001@hotmail.com>
2022-12-21 09:28:50 +0100

    added responsive hamburger menu

--- Scripts/bastu.js
+++ Scripts/bastu.js
@@ -1,19 +1,36 @@
-const button = document.querySelectorAll('input[type="button"]')[0];
+const button = document.querySelectorAll('input[type="button"]')[0]
 console.log(button)
 
-const selectElement = document.querySelector("#bokaBastu");
-console.log(selectElement);
+const selectElement = document.querySelector("#bokaBastu")
+console.log(selectElement)
 
 let bastu = localStorage.getItem("bastur")
-bastu = JSON.parse(bastu);
+bastu = JSON.parse(bastu)
 
 for (let i = 0; i < bastu.length; i++) {
     if (bastu[i] == "ledig") {
-        let newOption = new Option("Bastu " + (i + 1), (i + 1));
-        selectElement.add(newOption, undefined);
+        let newOption = new Option("Bastu " + (i + 1), (i + 1))
+        selectElement.add(newOption, undefined)
     }
 }
 
 document.querySelectorAll('input[type="button"]')[0].onclick = function() {
     console.log("clicked")
+}
+
+let isClicked = false
+const menu = document.querySelectorAll("#hamburger img")[0]
+
+menu.onclick = function() {
+    if (isClicked) {
+        document.querySelector("nav").style.display = "none"
+        document.querySelector(".nav-links").style.display = "none"
+        isClicked = false
+    }
+    else {
+        console.log("gdrgj")
+        document.querySelector("nav").style.display = "inline"
+        document.querySelector(".nav-links").style.display = "inline"
+        isClicked = true
+    }
 }
--- Scripts/omkl.js
+++ Scripts/omkl.js
@@ -1,19 +1,36 @@
-const button = document.querySelectorAll('input[type="button"]')[0];
+const button = document.querySelectorAll('input[type="button"]')[0]
 console.log(button)
 
-const selectElement = document.querySelector("#bokaBana");
-console.log(selectElement);
+const selectElement = document.querySelector("#bokaBana")
+console.log(selectElement)
 
 let banor = localStorage.getItem("banor")
-banor = JSON.parse(banor);
+banor = JSON.parse(banor)
 
 for (let i = 0; i < banor.length; i++) {
     if (banor[i] == "ledig") {
-        let newOption = new Option("Bana " + (i + 1), (i + 1));
-        selectElement.add(newOption, undefined);
+        let newOption = new Option("Bana " + (i + 1), (i + 1))
+        selectElement.add(newOption, undefined)
     }
 }
 
 document.querySelectorAll('input[type="button"]')[0].onclick = function() {
     console.log("clicked")
+}
+
+let isClicked = false
+const menu = document.querySelectorAll("#hamburger img")[0]
+
+menu.onclick = function() {
+    if (isClicked) {
+        document.querySelector("nav").style.display = "none"
+        document.querySelector(".nav-links").style.display = "none"
+        isClicked = false
+    }
+    else {
+        console.log("gdrgj")
+        document.querySelector("nav").style.display = "inline"
+        document.querySelector(".nav-links").style.display = "inline"
+        isClicked = true
+    }
 }
