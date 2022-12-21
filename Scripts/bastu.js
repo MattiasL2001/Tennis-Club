90b0f0730a870a396fef9091cfd071854b99c633
Mattias Lindblad <mattias-lindblad2001@hotmail.com>
2022-12-21 05:43:27 +0100

    added some javascript validation

--- Scripts/bastu.js
+++ Scripts/bastu.js
@@ -8,8 +8,10 @@ let bastu = localStorage.getItem("bastur")
 bastu = JSON.parse(bastu);
 
 for (let i = 0; i < bastu.length; i++) {
-    let newOption = new Option("Bastu " + (i + 1), (i + 1));
-    selectElement.add(newOption, undefined);
+    if (bastu[i] == "ledig") {
+        let newOption = new Option("Bastu " + (i + 1), (i + 1));
+        selectElement.add(newOption, undefined);
+    }
 }
 
 document.querySelectorAll('input[type="button"]')[0].onclick = function() {
--- Scripts/omkl.js
+++ Scripts/omkl.js
@@ -8,8 +8,10 @@ let banor = localStorage.getItem("banor")
 banor = JSON.parse(banor);
 
 for (let i = 0; i < banor.length; i++) {
-    let newOption = new Option("Bana " + (i + 1), (i + 1));
-    selectElement.add(newOption, undefined);
+    if (banor[i] == "ledig") {
+        let newOption = new Option("Bana " + (i + 1), (i + 1));
+        selectElement.add(newOption, undefined);
+    }
 }
 
 document.querySelectorAll('input[type="button"]')[0].onclick = function() {
